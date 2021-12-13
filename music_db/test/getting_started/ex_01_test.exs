fcb8b742fd88acacd7c16e3506486cb76c2eae94
a.werle <anatolij.werle@vitasystems.de>
2021-12-13 22:07:01 +0100

    add tests for the getting_started_01 examples

---
+++ music_db/test/getting_started/ex_01_test.exs
@@ -0,0 +1,22 @@
+defmodule GettingStarted.Ex01Test do
+  use MusicDB.DataCase
+  alias MusicDB.Artist
+
+  test "interacting with repo using schemas" do
+    assert {:ok, inserted = %Artist{}} = Repo.insert(%Artist{name: "Dizzy Gillespie"})
+
+    dizzy = Repo.get_by(Artist, name: "Dizzy Gillespie")
+    assert inserted.id == dizzy.id
+
+    {:ok, updated} = Repo.update(Ecto.Changeset.change(dizzy, name: "John Birks Gillespie"))
+    assert updated.name == "John Birks Gillespie"
+
+    dizzy = Repo.get_by(Artist, name: "John Birks Gillespie")
+    assert dizzy.id == inserted.id
+
+    {:ok, deleted} = Repo.delete(dizzy)
+    assert deleted.id == inserted.id
+
+    assert nil == Repo.get_by(Artist, name: "John Birks Gillespie")
+  end
+end
