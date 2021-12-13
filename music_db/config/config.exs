c60728662d853bd459979254a9c88767bc3de8a2
a.werle <anatolij.werle@vitasystems.de>
2021-12-13 21:13:22 +0100

    set configs

--- music_db/config/config.exs
+++ music_db/config/config.exs
@@ -1,14 +1,14 @@
-#---
+# ---
 # Excerpted from "Programming Ecto",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/wmecto for more book information.
-#---
+# ---
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
@@ -32,13 +32,15 @@ use Mix.Config
 config :music_db, :ecto_repos, [MusicDB.Repo]
 
 config :music_db, MusicDB.Repo,
-  # username: your_username,
-  # password: your_password,
+  username: "postgres",
+  password: "postgres",
   database: "music_db",
   hostname: "localhost",
-  migration_lock: nil # this is not normally needed - we put it here to support an example of
-                      # creating an index with the `concurrently` option set to true
+  # this is not normally needed - we put it here to support an example of
+  migration_lock: nil
+
+# creating an index with the `concurrently` option set to true
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
