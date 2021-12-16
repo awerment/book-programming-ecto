1c3a689345579cf7d161900f964a64c657469f14
a.werle <anatolij.werle@vitasystems.de>
2021-12-16 12:52:18 +0100

    add a fail! function to SearchEngine to simulate failure

--- music_db/lib/music_db/search_engine.ex
+++ music_db/lib/music_db/search_engine.ex
@@ -1,15 +1,19 @@
-#---
+# ---
 # Excerpted from "Programming Ecto",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/wmecto for more book information.
-#---
+# ---
 defmodule SearchEngine do
   def update!(item) do
     # search engine logic happens here...
     {:ok, item}
+  end
+
+  def fail!(_item) do
+    raise "Failed to update search engine"
   end
 
   def update(item) do
