aad01fe4fc116d88484235146438314ef19afee0
a.werle <anatolij.werle@vitasystems.de>
2021-12-16 15:45:27 +0100

    add Repo to the app's supervision tree

--- adding_ecto_to_app/lib/adding_ecto_to_app/application.ex
+++ adding_ecto_to_app/lib/adding_ecto_to_app/application.ex
@@ -8,8 +8,7 @@ defmodule AddingEctoToApp.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: AddingEctoToApp.Worker.start_link(arg)
-      # {AddingEctoToApp.Worker, arg}
+      AddingEctoToApp.Repo
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
